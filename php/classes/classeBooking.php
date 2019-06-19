3343021e958440859a875e02682130f4df0a62ea
fatihkoyuncu <51905401+fatihkoyuncu@users.noreply.github.com>
2019-06-19 17:33:13 +0200

    debutbdd

---
+++ php/classes/classeBooking.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../database/database.php");
+class Booking {
+    
+    
+}
---
+++ php/database/database.php
@@ -0,0 +1,15 @@
+<?php
+require_once("config.php");
+
+function dbConnect(){
+    try{
+        $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
+    }
+    catch (PDOException $exception){
+        header("HTTP/1.1 503 Service unavailable");
+        //error_log('Connection error: '.$exception->getMessage());
+        return false;
+    }
+    return $db;
+}
+?>
